cffccaea0eac8df6fc8b73e9672ac23f48f622f3
Shivamt2107 <shivam.gote2107@gmail.com>
2023-04-24 22:20:47 +0530

    add file

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -48,44 +48,70 @@ const ProjectCard = () => {
 
   const projects = [
     {
-      img: "/projects/himalaya.png",
+      img: "/projects/Himalaya.png",
       name: "Himalaya-Usa Clone.",
-      tech: ['HTML', ' CSS', 'JavaScript', ' REST API', 'Icon'],
+      tech: ["HTML", " CSS", "JavaScript", " REST API", "Icon"],
       link: "https://630c7ea5fe01ec7d8a7acb5f--aquamarine-khapse-1fb54c.netlify.app/",
-      dec: ' Himalaya USA is a website selling wellness products in the United States of America.',
-      code: 'https://github.com/raj2820/Himalya-USA-Clone'
+      dec: " Himalaya USA is a website selling wellness products in the United States of America.",
+      code: "https://github.com/raj2820/Himalya-USA-Clone",
     },
     {
       img: "/projects/weefit.png",
       name: "WEFIT-Clone",
-      tech: ["React JS", "JavaScript XML", "Redux", "Axios", "Redux-thunk", "React-Router", "Chakra-UI"],
+      tech: [
+        "React JS",
+        "JavaScript XML",
+        "Redux",
+        "Axios",
+        "Redux-thunk",
+        "React-Router",
+        "Chakra-UI",
+      ],
       link: "https://we-fit.netlify.app/",
       dec: "WEFIT is a body fitness website where users can choose from professionally built workout plans.",
-      code: "https://github.com/vaibhav-mougha/WEFIT"
+      code: "https://github.com/vaibhav-mougha/WEFIT",
     },
     {
       img: "/projects/weesho.png",
       name: "Meesho-Clone",
-      tech: ["React", "Redux", "Axios", "JavaScript XML", "HTML", "CSS", "Material-UI", "Tailwind"],
+      tech: [
+        "React",
+        "Redux",
+        "Axios",
+        "JavaScript XML",
+        "HTML",
+        "CSS",
+        "Material-UI",
+        "Tailwind",
+      ],
       link: "https://fasion-hub-store.vercel.app/",
       dec: "Meesho is a unique platform that allows users to sell products online through social media platforms. ",
-      code: "https://github.com/sumitss085/FashionHub_store"
+      code: "https://github.com/sumitss085/FashionHub_store",
     },
     {
       img: "/projects/LensKart.png",
       name: "LensKart-Clone",
-      tech: ["React", "Redux", "Axios", "JavaScript XML", "Mango", "CSS", "Material-UI", "Tailwind"],
+      tech: [
+        "React",
+        "Redux",
+        "Axios",
+        "JavaScript XML",
+        "Mango",
+        "CSS",
+        "Material-UI",
+        "Tailwind",
+      ],
       link: "https://fascinating-cajeta-e3c16d.netlify.app/",
       dec: "LensKart is leading e-commerce company that online optical store packed classic and trendy eyeglasses. ",
-      code: "https://github.com/Shivamt2107/LENSKART-CLONE"
+      code: "https://github.com/Shivamt2107/LENSKART-CLONE",
     },
     {
       img: "/projects/myfitness.png",
       name: "MyFitnessPal-Clone",
-      tech: ["React", "JavaScript XML", "HTML", "CSS", 'Chakra-UI'],
+      tech: ["React", "JavaScript XML", "HTML", "CSS", "Chakra-UI"],
       link: "https://reliable-kataifi-f68fe6.netlify.app/",
       dec: "MyFitnessPal allows users to track calories, monitor progress toward weight-management goals.",
-      code: "https://github.com/Shivamt2107/expert-hand-9078/tree/main/shivam-gym"
+      code: "https://github.com/Shivamt2107/expert-hand-9078/tree/main/shivam-gym",
     },
     {
       img: "/projects/revv.png",
@@ -93,7 +119,7 @@ const ProjectCard = () => {
       tech: ["React", "JavaScript XML", "HTML", "CSS"],
       link: "https://lustrous-kangaroo-e975a7.netlify.app/",
       dec: "Revv Cars is an Indian self-drive mobility start up funded by Hyundai and others. The company was founded by Anupam Agarwal and Karan Jain in July 2015.",
-      code: "https://github.com/Shivamt2107/unique-knowledge-3204"
+      code: "https://github.com/Shivamt2107/unique-knowledge-3204",
     },
     {
       img: "/projects/youtube.png",
@@ -101,17 +127,17 @@ const ProjectCard = () => {
       tech: ["javaScript", "HTML", "CSS", "Api"],
       link: "https://rococo-empanada-4c6ee0.netlify.app/",
       dec: "YouTube is a global online video sharing and social media platform also YouTube is a video sharing service where users can watch, like, share, comment and upload their own videos.",
-      code: "https://github.com/masai-course/shivam_Fw19_1173/tree/master/unit-3/sprint-3/day-1/assignments"
+      code: "https://github.com/masai-course/shivam_Fw19_1173/tree/master/unit-3/sprint-3/day-1/assignments",
     },
     {
       img: "/projects/movie.png",
       name: "Movie Search",
-      tech: ['HTML', 'Css', 'javaScript', 'Api'],
+      tech: ["HTML", "Css", "javaScript", "Api"],
       link: "https://meek-praline-19b06a.netlify.app/",
       dec: "It is Simple Movie Search app using a IMDB Api its the world's most popular and search api (imdb) source for movie, TV and celebrity content. Search ratings and reviews for the newest movie and TV shows.",
-      code: "https://github.com/shivamt2107"
+      code: "https://github.com/shivamt2107",
     },
-    
+
     // {
     //   img: "/projects/weather.png",
     //   name: "Weather",
@@ -136,8 +162,6 @@ const ProjectCard = () => {
     //   dec: "It is Simple-TODO-APP",
     //   code: "https://github.com/shivamt2107"
     // },
-
-
   ];
 
   return (
